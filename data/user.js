e94814303f94e239d2bcb2faaf055a642545fbab
Wisnu Prayoga <wisnuprayoga@Wisnus-MacBook-Pro.local>
2018-11-18 02:09:32 +0700

    update role login

---
+++ data/user.js
@@ -0,0 +1,30 @@
+var data = {
+  users:
+  [
+    {
+      username: 'pengadaan@ui.ac.id',
+      name: 'Staff Fasilitas Umum',
+      role: 'pengadaan'
+    },
+    {
+      username: 'admin@ui.ac.id',
+      name: 'Admin Aset TI',
+      role: 'admin'
+    },
+    {
+      username: 'helpdesk@ui.ac.id',
+      name: 'Helpdesk',
+      role: 'helpdesk'
+    },
+    {
+      username: 'pemelihara@ui.ac.id',
+      name: 'Pemelihara Aset TI',
+      role: 'pemelihara'
+    },
+    {
+      username: 'penanggung@ui.ac.id',
+      name: 'Penanggung Jawab Ruangan',
+      role: 'penanggung'
+    }
+  ]
+}
